3f418bb769a3f9934db4f32b9cebaffeb8bea543
CharlesXu0715 <xuyuantaoxyt@gmail.com>
2022-07-13 03:11:44 +0200

    Fix remove marker bug
    with code to load json files from local files (functionality not yet finished)

--- app/src/main/java/com/example/visual/MapsActivity.java
+++ app/src/main/java/com/example/visual/MapsActivity.java
@@ -1,9 +1,18 @@
 package com.example.visual;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +24,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.visual.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.model.BitmapDescriptor;
@@ -29,12 +39,21 @@ import com.google.maps.android.data.geojson.GeoJsonPointStyle;
 import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,View.OnClickListener {
+
+    private static final String TAG = "TAG";
+    private static final int FILE_SELECTOR_CODE = 42;
+    private final static String mLogTag = "GeoJsonVisualise";
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -42,8 +61,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Button mImportButton;
     private Button mDeleteButton;
     private GeoJsonLayer layer;
+    private String filePath;
 
-    private final static String mLogTag = "GeoJsonVisualise";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +70,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        requestReadPermission();    //request permission for load local json files
 
         mGreetingTextView = findViewById(R.id.maps_textview_greeting);
         mImportButton = findViewById(R.id.maps_button_import);
@@ -80,21 +101,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mImportButton.setOnClickListener(this);
         mDeleteButton.setOnClickListener(this);
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     //create geojson layer via the file local and load it on the map
     private void createLayer() {
         try {
+            //load file stocked in raw
             layer = new GeoJsonLayer(mMap, R.raw.geojsonfile1, this);
-            GeoJsonPolygonStyle geoJsonPolygonStyle = layer.getDefaultPolygonStyle();
-            geoJsonPolygonStyle.setStrokeWidth(10);
-            geoJsonPolygonStyle.setStrokeColor(Color.RED);
             addGeoJsonLayerToMap(layer);
-
         } catch (IOException e) {
             Log.e(mLogTag, "GeoJSON file could not be read");
         } catch (JSONException e) {
@@ -121,41 +135,86 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void addGeoJsonLayerToMap(GeoJsonLayer layer) {
-
-//        addColorsToMarkers(layer);
+        addColorToLayer(layer);
         layer.addLayerToMap();
-        // Demonstrate receiving features via GeoJsonLayer clicks.
-//        layer.setOnFeatureClickListener(new GeoJsonLayer.GeoJsonOnFeatureClickListener() {
-//            @Override
-//            public void onFeatureClick(Feature feature) {
-//                Toast.makeText(MapsActivity.this,
-//                        "Feature clicked: " + feature.getProperty("title"),
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//        });
+        //Demonstrate receiving features via GeoJsonLayer clicks
+        layer.setOnFeatureClickListener(new GeoJsonLayer.GeoJsonOnFeatureClickListener() {
+            @Override
+            public void onFeatureClick(Feature feature) {
+                Toast.makeText(MapsActivity.this,
+                        "Commune:" + feature.getProperty("commune"),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 
-    private void addColorsToMarkers(GeoJsonLayer layer) {
-
-        for (GeoJsonFeature feature : layer.getFeatures()) {
-            if (feature.getGeometry()!=null) {
-                Log.e("info",feature.getGeometry().toString());
-            }
-        }
+    private void addColorToLayer(GeoJsonLayer layer) {
+        GeoJsonPolygonStyle geoJsonPolygonStyle = layer.getDefaultPolygonStyle();
+        geoJsonPolygonStyle.setStrokeWidth(10);
+        geoJsonPolygonStyle.setStrokeColor(Color.RED);
     }
 
     @Override
     public void onClick(View view) {
         if (view == mImportButton) {
+            //from files in raw
             createLayer();
+
+            //from files in storage
+//            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//            intent.setType("*/*");
+//            intent.addCategory(Intent.CATEGORY_OPENABLE);
+//            startActivityForResult(intent, FILE_SELECTOR_CODE);
+
             mImportButton.setEnabled(false);
             mDeleteButton.setEnabled(true);
         }
         else if (view == mDeleteButton) {
             layer.removeLayerFromMap();
+            mMap.clear();
             mDeleteButton.setEnabled(false);
             mImportButton.setEnabled(true);
         }
     }
+
+    private void requestReadPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //request
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        } else {
+            Log.d(TAG, "requestMyPermissions: permission granted");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FILE_SELECTOR_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getData();
+            Toast.makeText(this, "path：" + uri.toString(), Toast.LENGTH_SHORT).show();
+            filePath=uri.getPath().toString();
+            Log.e("path",filePath);
+            try {
+                FileInputStream fin = new FileInputStream(filePath);
+                int length = fin.available();
+                byte[] buffer = new byte[length];
+                fin.read(buffer);
+                String content = new String(buffer);
+                JSONObject jsonObject=new JSONObject(content);
+                layer = new GeoJsonLayer(mMap,jsonObject);
+                addGeoJsonLayerToMap(layer);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
